19e5fe0d45c1f9ede8e8f7e1c3e1c3673d3ede33
agolebiowska <agataxgolebiowska@gmail.com>
2020-02-02 00:30:44 +0100

    pretty doc

--- internal/api/api.go
+++ internal/api/api.go
@@ -9,7 +9,15 @@ import (
 	"github.com/agolebiowska/QWdhdGEgR29sZWJpb3dza2EgcmVjcnVpdG1lbnQgdGFzaw/pkg/openweather"
 )
 
-// GET /api/v1/weather/current?q=warsaw,london
+/*
+	GET /api/v1/weather/current
+
+	required:
+		q, example=warsaw,london,tokyo
+	optional:
+		limit, default=20
+		page, default=1
+*/
 func CurrentWeather(conf *config.Config) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		c := openweather.NewClient(conf)
